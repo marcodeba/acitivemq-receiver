88de33f3048c400f16efd986c6f3b73433df2826
panning255 <panning255@jk.cn>
2019-01-11 01:42:39 +0800

    remove comments

--- src/main/java/com/gupaoedu/activemq/JMSQueueListenerConsumer.java
+++ src/main/java/com/gupaoedu/activemq/JMSQueueListenerConsumer.java
@@ -5,11 +5,6 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 import java.io.IOException;
 
-/**
- * 腾讯课堂搜索 咕泡学院
- * 加群获取视频：608583947
- * 风骚的Michael 老师
- */
 public class JMSQueueListenerConsumer {
 
     public static void main(String[] args) {
@@ -46,9 +41,6 @@ public class JMSQueueListenerConsumer {
 
             TextMessage textMessage = (TextMessage) consumer.receive();
             System.out.println(textMessage.getText());
-
-//            session.commit();
-//            session.close();
         } catch (JMSException e) {
             e.printStackTrace();
         } catch (IOException e) {
